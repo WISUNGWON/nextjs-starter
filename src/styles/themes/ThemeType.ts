08ddb9e9d2558232c93a15a3d57032b0075dd30c
Sungwon WI <52841715+WISUNGWON@users.noreply.github.com>
2022-02-10 00:36:17 +0900

    feat: theme 추가

--- src/styles/themes/ThemeType.ts
+++ src/styles/themes/ThemeType.ts
@@ -3,9 +3,13 @@ export interface ThemeType {
     white: string;
 
     gray5: string;
-
+    slategray10: string;
+    slategray20: string;
+    slategray40: string;
+    slategray60: string;
+    slategray80: string;
     blue5: string;
-
+    primary: string;
     yellow10: string;
 
     pink10: string;
--- src/styles/themes/theme.ts
+++ src/styles/themes/theme.ts
@@ -3,11 +3,15 @@ export const theme = {
     white: "#FFFFFF",
 
     gray5: "#FAFAFB",
-
+    slategray10: "#F2F4F6",
+    slategray20: "#E2E4E8",
+    slategray40: "#B0B7BC",
+    slategray60: "#646F7C",
+    slategray80: "#393E43",
     blue5: "#F1F3FF",
 
     yellow10: "#F2CF8A",
-
+    primary: "#00B140",
     pink10: "#FCF0F1",
   },
 };
