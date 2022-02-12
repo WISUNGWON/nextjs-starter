6f387077c3e2053683ec80c0e78bb4748ee22116
WISUNGWON <tkrhkaos12@naver.com>
2022-02-12 16:11:55 +0900

    config(test): unsplash kep 추가

--- src/core/api/unsplash.ts
+++ src/core/api/unsplash.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: `https://api.unsplash.com`,
   headers: {
-    Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`,
+    Authorization: `Client-ID -2jryNcKIglfjibj8-7aD02k04pJL9FvKI_LM-TZ8jQ`,
   },
 });
